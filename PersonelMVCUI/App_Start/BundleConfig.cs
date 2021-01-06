cb17abe1454a4b05d3ddcd4b77949c85b7193a5c
brkmck <brkmck1@gmail.com>
2021-01-06 22:31:19 +0300

    Initial commit

---
+++ PersonelMVCUI/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace PersonelMVCUI.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-1.10.2.js",
+                "~/Scripts/jquery.unobtrusive-ajax.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/DataTables/jquery.dataTables.min.js",
+                "~/Scripts/DataTables/dataTables.bootstrap.min.js",
+                "~/Scripts/custom.js",
+                "~/Scripts/bootbox.min.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/site2.css",
+                "~/Content/DataTables/css/dataTables.bootstrap.min.css"
+                ));
+        }
+    }
+}
